56e35cd02b593a4aeecb879f89980caec472adc0
Mireia <mireiagonzalezz03@gmail.com>
2025-08-15 20:38:22 +0200

    build turret when clicking on a gridmap cell

---
+++ turret_manager.gd
@@ -0,0 +1,12 @@
+class_name TurretManager extends Node3D
+
+@export var turret: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func build_turret(turret_position: Vector3) -> void:
+	var new_turret = turret.instantiate()
+	add_child(new_turret)
+	new_turret.global_position = turret_position
