c147d459ce8487f579fbc3e8e7f556f73f16d112
Mireia <mireiagonzalezz03@gmail.com>
2025-08-15 21:03:23 +0200

    created projectile and shoot it from the top of the turret

---
+++ Turret/projectile.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+@export var speed: float = 30
+
+var direction: Vector3 = Vector3.FORWARD
+
+func _physics_process(delta: float) -> void:
+	position += direction * speed * delta
+
+func _on_timer_timeout() -> void:
+	queue_free()
