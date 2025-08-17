daacb53a3ff601161e0511473cb0fcc3cc2859db
Mireia <mireiagonzalezz03@gmail.com>
2025-08-17 14:27:17 +0200

    create difficulty manager to control enemy spawn time

---
+++ DifficultyManager/difficulty_manager.gd
@@ -0,0 +1,15 @@
+class_name DifficultyManager extends Node
+
+@export var game_length: float  = 30.0
+@export var spawn_time_curve: Curve 
+
+@onready var timer: Timer = $Timer
+
+func _ready() -> void:
+	timer.start(game_length)
+
+func game_progress_ratio() ->  float:
+	return 1.0 - (timer.time_left / game_length)
+
+func get_spawn_time() -> float:
+	return spawn_time_curve.sample(game_progress_ratio())
